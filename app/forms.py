7e52d682ef4847ef78559d57caf37e893ee9bf3c
Andrew Lange <andrew.lange93@gmail.com>
2019-02-23 15:44:00 -0500

    login form field validation

--- app/forms.py
+++ app/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
-    # create a StringField with label 'Username' 
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
