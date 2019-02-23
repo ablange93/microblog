023311d338cc847f5cad5b40ee00ee43c30b11d9
Andrew Lange <andrew.lange93@gmail.com>
2019-02-23 15:26:51 -0500

    Login view function, form object, html template

---
+++ app/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    # create a StringField with label 'Username' 
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
