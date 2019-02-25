38d071c7fbdccf129f00b5966625fb905395e361
Andrew Lange <andrew.lange93@gmail.com>
2019-02-24 20:06:51 -0500

    @login_required decorator prevents unauthorized users from accessing index.html

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
-from flask_login import logout_user
+from flask_login import logout_user, login_required
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
