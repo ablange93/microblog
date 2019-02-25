497fb641e8dfbf252895ef2f8d945cedeabf1bcd
Andrew Lange <andrew.lange93@gmail.com>
2019-02-25 03:08:09 -0500

    import hashlib md5

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
 class User(UserMixin ,db.Model):
     id = db.Column(db.Integer, primary_key=True)
